00a14d5a7d78abca40783aed36e2ecc34aebd004
Ivaylo Marinov <shmingela@gmail.com>
2020-01-04 21:22:46 +0100

    added x,y,z calculation added euclidean distance calculation

--- main.c
+++ main.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <tgmath.h>
 #include "merge_sort.h"
+
+#define POINTS 1000
+#define RADIUS 6371
+#define PI 3.14159265
 
 struct datapoint {
     char event[30]; // event information
     char datetime[30]; // date and time information
     double latitude; // latitude information (Breitengrad)
     double longitude; // longtitude information (Laengengrad)
+    double x;
+    double y;
+    double z;
     int altitude; // Geopotential height above sea level integrated from pressure, temperature and humidity
     int hhh; // Geopotential height above sea level derived from GPS receiver mounted on radiosonde
     int hgeom1; //
@@ -20,6 +28,10 @@ struct datapoint {
     double abstand;
 };
 
+void xyz_calc(struct datapoint *dp);
+void euclidean_dist(struct datapoint *dp1, struct datapoint *dp2);
+
+/*****************************************************MAIN***********************************************************/
 int main() {
     // gets time at start
     clock_t begin = clock();
@@ -29,9 +41,9 @@ int main() {
     //int ArrayToSort[12]={3,2,1,8,9,7,0,4,10,6,5,11};
     init_f(ArrayToSort, array_size);
     ausgabe(ArrayToSort, array_size);
-    Merge_sort(ArrayToSort, 0, array_size - 1); // es werden die Elemente 1 bis 9 und nicht 0 bis 9 sortiert
+    Merge_sort(ArrayToSort, 0, array_size - 1); // es werden die Elemente 0 bis (array_size-1) sortiert
     ausgabe(ArrayToSort, array_size);
-
+    free(ArrayToSort);
 
     char fileNameString[] = "/home/ivaylo/Desktop/GDI-PROJEKT/DataToRead.tab";
     FILE *fileToRead;
@@ -49,33 +61,41 @@ int main() {
     FILE *fileToWrite;
     fileToWrite = fopen(writeFile, "w");
 
-    struct datapoint dp[10];
+    struct datapoint dp[POINTS];
+
+    int fail = 0;
+    int success;
+    int prev = 0;
     int i = 0;
-
-
-    for (i = 0; i < 10; i++) {
-        fscanf(fileToRead, "%s\t%s\t%lf\t%lf\t%d\t%d\t%d\t%d\t%lf\t%lf\t%lf\t%d\t%lf\n", &dp[i].event, dp[i].datetime,
-               &dp[i].latitude, &dp[i].longitude,
-               &dp[i].altitude, &dp[i].hhh, &dp[i].hgeom1, &dp[i].hgeom2, &dp[i].PPPP, &dp[i].TTT, &dp[i].RH, &dp[i].dd,
-               &dp[i].ff);
+    // reads the chosen amount of data points and deletes any points with missing values
+    for (i = 0; i < POINTS; i++) {
+        success = fscanf(fileToRead, "%s\t%s\t%lf\t%lf\t%d\t%d\t%d\t%d\t%lf\t%lf\t%lf\t%d\t%lf\n", dp[i].event,
+                         dp[i].datetime, &dp[i].latitude, &dp[i].longitude, &dp[i].altitude, &dp[i].hhh,
+                         &dp[i].hgeom1,
+                         &dp[i].hgeom2, &dp[i].PPPP, &dp[i].TTT, &dp[i].RH, &dp[i].dd, &dp[i].ff);
+        if (success != 13)fail = 1;
+        if (success != 13 || fail != 0 || prev != 0) {
+            i--;
+            prev = fail;
+            fail = 0;
+        }
     }
-
-    printf("\nThe read values are: \n");
-    for (i = 0; i < 10; i++) {
-        /*printf("dp:%d\t%f\t%f\t%d\t%d\t%d\t%d\t%f\t%f\t%f\t%d\t%f\n", i, dp[i].latitude, dp[i].longitude,
-               dp[i].altitude, dp[i].hhh, dp[i].hgeom1, dp[i].hgeom2, dp[i].PPPP, dp[i].TTT, dp[i].RH, dp[i].dd,
-               dp[i].ff);
-        */
-        fprintf(fileToWrite, "dp:%d\t%s\t%s\t%.5f\t%.5f\t%d\t%d\t%d\t%d\t%.2f\t%.2f\t%.1f\t%d\t%.1f\n", i, dp[i].event,
+    // puts all of the read data in a new file with the number of the point in front of the values
+    for (i = 0; i < POINTS; i++) {
+        fprintf(fileToWrite, "dp:%d\t%s\t%s\t%.5lf\t%.5lf\t%d\t%d\t%d\t%d\t%.2lf\t%.2lf\t%.1lf\t%d\t%.1lf\n", i,
+                dp[i].event,
                 dp[i].datetime, dp[i].latitude, dp[i].longitude,
                 dp[i].altitude, dp[i].hhh, dp[i].hgeom1, dp[i].hgeom2, dp[i].PPPP, dp[i].TTT, dp[i].RH, dp[i].dd,
                 dp[i].ff);
     }
-
+    xyz_calc(&dp[3]);
+    printf("x: %lf\ny: %lf\nz: %lf\nLongtitude: %lf\nLatitude: %lf\n",dp[3].x,dp[3].y,dp[3].z,dp[3].longitude,dp[3].latitude);
+    euclidean_dist(&dp[0],&dp[999]);
+    printf("%.2lf km",dp[0].abstand);
     fclose(fileToRead);
     fclose(fileToWrite);
-    free(ArrayToSort);
-    // gets time at end
+
+
     clock_t end = clock();
     double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
     printf("\nExecution time: %lf sec\n", time_spent);
@@ -85,6 +105,32 @@ int main() {
      */
     return 0;
 }
+/***************************************************END OF MAIN******************************************************/
 
+void xyz_calc(struct datapoint *dp) {
+    double degToRad = 0.017453293;
+    double betaRad,alfaRad;
+    betaRad=(90 - dp->longitude)*degToRad;
+    alfaRad=dp->latitude*degToRad;
+    dp->x = RADIUS * sin(betaRad) * cos(alfaRad);
+    dp->y = RADIUS * sin(betaRad) * sin(alfaRad);
+    dp->z = RADIUS * cos(betaRad);
+}
 
+/* Function to calculate euclidean distance between two points.
+ * Distance will be saved in the abstand value of the first data point(dp1)
+ * dp1 - the first of the both data points between which the distance will be calculated
+ * dp2 - the second data point */
+void euclidean_dist(struct datapoint *dp1, struct datapoint *dp2){
+    xyz_calc(dp1);
+    xyz_calc(dp2);
+    /*printf("(x1 - x2)² = (%lf - %lf)² = %lf\n",dp1->x,dp2->x,pow(dp1->x-dp2->x,2) );
+     * printf("(x1 - x2)² = (%lf - %lf)² = %lf\n",dp1->y,dp2->y,pow(dp1->y-dp2->y,2) );
+     * printf("(x1 - x2)² = (%lf - %lf)² = %lf\n",dp1->z,dp2->z,pow(dp1->z-dp2->z,2) );
+     */
+    dp1->abstand=sqrt(pow(dp1->x-dp2->x,2) + pow(dp1->y - dp2->y,2) + pow(dp1->z - dp2->z,2) );
+}
 
+void orthodromic_dist(){
+
+}
